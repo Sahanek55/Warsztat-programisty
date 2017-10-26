45db2165316b286fd903ac32d46d6bf528d9fcc5
serwis <imserwis@s0053-06.stud.matinf.uj.edu.pl>
2017-10-26 09:28:03 +0200

    sortowanie z wypisywaniem co przejście

--- plik2.cpp
+++ plik2.cpp
@@ -6,31 +6,35 @@ using namespace std;
 void sort(int tab[],int n)
 {int temp=0;
 		for(int x=0;x<n;x++)
-	for(int y=0;y<n-1;y++)
+	{for(int y=0;y<n-1;y++)
 	if(tab[y]>tab[y+1])
 	{temp=tab[y];
 	tab[y]=tab[y+1];
 	tab[y+1]=temp;}	
+	for(int y=0;y<n;y++)
+	cout<<tab[y]<<" ";
+	cout<<endl;
+	}
 	
 }
 
 int main ()
 
 {
-	int tab [100];
+	int tab [10];
 	
 	srand(time(NULL));
 	
-	for(int x=0;x<100;x++)
-	tab[x]=rand()%100;
+	for(int x=0;x<10;x++)
+	tab[x]=rand()%10;
 	
-	for(int x=0;x<100;x++)
+	for(int x=0;x<10;x++)
 	cout<<tab[x]<<endl;
 	
 	cout<<endl;
-	sort(tab,100);
+	sort(tab,10);
 	
-	for(int x=0;x<100;x++)
+	for(int x=0;x<10;x++)
 	cout<<tab[x]<<endl;
 	
 	
